f4c6d50e059f497386499b1c558ba0b9a1f0c17f
Shivang Kushwaha <shivangkushwaha025@gmail.com>
2024-06-12 23:46:24 +0530

    Added server error handling in user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,8 @@ exports.getUsers = async (req, res) => {
         const users = await User.find();
         res.status(200).json(users);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        console.error('Error in User:getUsers', error)
+        return sendServerErrorResponse(res, error)
     }
 };
 
@@ -21,7 +22,8 @@ exports.createUser = async (req, res) => {
         const newUser = await user.save();
         res.status(201).json(newUser);
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        console.error('Error in User:createUser', error)
+        return sendServerErrorResponse(res, error)
     }
 };
 
@@ -39,7 +41,8 @@ exports.updateUser = async (req, res) => {
         }
         res.status(200).json(user);
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        console.error('Error in User:updateUser', error)
+        return sendServerErrorResponse(res, error)
     }
 };
 
@@ -52,7 +55,8 @@ exports.deleteUser = async (req, res) => {
         }
         res.status(200).json({ message: 'User deleted' });
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        console.error('Error in User:deleteUser', error)
+        return sendServerErrorResponse(res, error)
     }
 };
 
