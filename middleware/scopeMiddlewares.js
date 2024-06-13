99c3598b3f7392ef7bdba722e802330a967fa3d3
Shivang Kushwaha <shivangkushwaha025@gmail.com>
2024-06-13 23:29:08 +0530

    Removed permission middleware from the scop

--- middleware/scopeMiddlewares.js
+++ middleware/scopeMiddlewares.js
@@ -1,25 +1,3 @@
-const Joi = require("joi");
-const { sendBadResponse } = require("../controllers/responseController");
-// const scopeValidator = ( scope = [] ) => {
-//   return (req, res, next) => {
-//     if( scope == null || scope.length === 0 )
-//         return next()
-//     let isValid = false;
-//     let userPermissions = req.auth.scope;
-//     for (const permission of userPermissions) {
-//         if( scope.includes(permission) ){
-//                 isValid = true
-//                 break;
-//         }
-//     }
-//     if ( isValid ) {
-//       next();
-//     } else {
-//       return sendBadResponse(res,"Premission out of scope", 422);
-//     }
-//   }
-// }
-
 const scopeValidator = (scope = []) => {
   return (resolver) => {
       return async (parent, args, context, info) => {
